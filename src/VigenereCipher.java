3b1181ee9054a1be196ba3260c3a657fdbe87d7c
juliansung <julians2025@student.cis.edu.hk>
2023-02-12 11:56:28 +0800

    b

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -1,36 +1,20 @@
 public class VigenereCipher {
-
     private String alphabet;
     private String key;
-
     public VigenereCipher(String key) {
         alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         this.key = key.toLowerCase();
     }
-
     public String getKey(){
         return key;
     }
-
-    public String setKey(){
-        this.key=key;
-        return key;
-    }
-
-    public String getAlphabet(){
-        return alphabet;
-    }
-
+    public String getAlphabet(){return alphabet;}
     public String encode(String input){
         StringBuilder output = new StringBuilder();
-        for(int i = 0; i<input.length(); i++){
-            char shift = key.charAt(i%key.length());
+        for(int i = 0; i < input.length(); i++){
+            char shift = key.charAt(i % key.length());
             boolean inAlphabet = false;
-            for(int j=0; j<52; j++){
-                if (alphabet.charAt(j) == input.charAt(i)) {
-                    inAlphabet = true;
-                }
-            }
+            for(int j = 0; j < 52; j++) if (alphabet.charAt(j) == input.charAt(i)) inAlphabet = true;
             if(!inAlphabet){
                 output.append(input.charAt(i));
                 continue;
@@ -38,23 +22,20 @@ public class VigenereCipher {
             int shifted = input.charAt(i) + shift - 'a';
             if (shifted > 'z') {
                 char newChar = (char) shifted;
-                while (newChar > 'z') {
-                    newChar -= 26;
-                }
+                while (newChar > 'z') newChar -= 26;
                 output.append(newChar);
             }
             else output.append((char) shifted);
-
         }
         return output.toString();
     }
 
     public String decode(String input){
         StringBuilder output = new StringBuilder();
-        for(int i = 0; i<input.length(); i++){
+        for(int i = 0; i < input.length(); i++){
             boolean inAlphabet = false;
-            char shift = key.charAt(i%key.length());
-            for(int j=0; j<52; j++){
+            char shift = key.charAt(i % key.length());
+            for(int j = 0; j < 52; j++){
                 if (alphabet.charAt(j) == input.charAt(i)) inAlphabet = true;
             }
             if(!inAlphabet){
@@ -64,13 +45,10 @@ public class VigenereCipher {
             int shifted = input.charAt(i) - shift + 'a';
             if (shifted < 'a') {
                 char newChar = (char) shifted;
-                while (newChar < 'a') {
-                    newChar += 26;
-                }
+                while (newChar < 'a') newChar += 26;
                 output.append(newChar);
             }
             else output.append((char) shifted);
-
         }
         return output.toString();
     }
